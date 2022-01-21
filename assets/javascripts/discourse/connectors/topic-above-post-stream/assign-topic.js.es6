03632feba3d2104aa680cf524d53633b2c20cac1
Jacob Shaw <jacob.shaw@genesys.com>
2022-01-21 17:19:35 -0500

    test moving actions to helper

---
+++ assets/javascripts/discourse/connectors/topic-above-post-stream/assign-topic.js.es6
@@ -0,0 +1,47 @@
+import { withPluginApi } from 'discourse/lib/plugin-api';
+
+export default {
+
+    filterUsersWithApi(api, opts) {
+      console.log('GOT IN FILTER ACTION');
+      const { topic } = opts;
+      const users = api.container.lookup('controller:users')?.get('model') || {};
+      console.log('USERS', users);
+      console.log('TOPIC', opts.topic);
+      const matchingUsers = this.users?.filter((user) => {
+          return user.username?.toLowerCase()?.includes(topic.custom_fields.search_term.toLowerCase());
+      });
+      const userSearchList = document.getElementById('user-search-list');
+
+      userSearchList.innerHTML = matchingUsers.map((u) => {
+        return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
+      }).join('\n');
+
+      for (u in matchingUsers) {
+        const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
+
+        userEl.addEventListener("click", function() {
+          topic.set('custom_fields.assigned_user', u.username);
+          topic.set('custom_fields.is_assigned', true);
+          topic.set('custom_fields.search_term', '')
+          const event = new Event('change');  
+          document.getElementById('user-search').dispatchEvent(event);
+        });
+      }
+    },
+    
+    // users: Ember.inject.controller("users"),
+
+    actions: {
+
+      filterUsers(topic) {
+        withPluginApi("0.8", filterUsersWithApi, { topic });
+      },
+      
+      unassignUser(topic) {
+        topic.set('custom.fields.assigned_user', undefined);
+        topic.set('custom_fields.is_assigned', false);
+      },
+
+    }
+};
--- assets/javascripts/discourse/initializers/assign-topic.js
+++ assets/javascripts/discourse/initializers/assign-topic.js
@@ -9,43 +9,45 @@ function initWithApi(api) {
 
   //const defaultSearchTerm = '';
 
-  api.modifyClass("controller:topic", {
-    // searchTerm: defaultSearchTerm,
-    users: Ember.inject.controller("users"),
+  // api.modifyClass("controller:topic", {
+  //   // searchTerm: defaultSearchTerm,
+  //   users: Ember.inject.controller("users"),
 
-    actions: {
+  //   actions: {
 
-      filterUsers(topic) {
-        console.log('INSIDE HELPER', users);
-        const matchingUsers = this.users?.filter((user) => {
-            return user.username?.toLowerCase()?.includes(topic.custom_fields.search_term.toLowerCase());
-        });
-        const userSearchList = document.getElementById('user-search-list');
+  //     filterUsers(topic) {
+  //       console.log('GOT IN FILTER ACTION')
+  //       console.log('USERS', users);
+  //       console.log('TOPIC', topic);
+  //       const matchingUsers = this.users?.filter((user) => {
+  //           return user.username?.toLowerCase()?.includes(topic.custom_fields.search_term.toLowerCase());
+  //       });
+  //       const userSearchList = document.getElementById('user-search-list');
 
-        userSearchList.innerHTML = matchingUsers.map((u) => {
-          return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
-        }).join('\n');
+  //       userSearchList.innerHTML = matchingUsers.map((u) => {
+  //         return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
+  //       }).join('\n');
 
-        for (u in matchingUsers) {
-          const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
+  //       for (u in matchingUsers) {
+  //         const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
 
-          userEl.addEventListener("click", function() {
-            topic.set('custom_fields.assigned_user', u.username);
-            topic.set('custom_fields.is_assigned', true);
-            topic.set('custom_fields.search_term', '')
-            const event = new Event('change');  
-            document.getElementById('user-search').dispatchEvent(event);
-          });
-        }
-      },
+  //         userEl.addEventListener("click", function() {
+  //           topic.set('custom_fields.assigned_user', u.username);
+  //           topic.set('custom_fields.is_assigned', true);
+  //           topic.set('custom_fields.search_term', '')
+  //           const event = new Event('change');  
+  //           document.getElementById('user-search').dispatchEvent(event);
+  //         });
+  //       }
+  //     },
       
-      unassignUser(topic) {
-        topic.set('custom.fields.assigned_user', undefined);
-        topic.set('custom_fields.is_assigned', false);
-      },
+  //     unassignUser(topic) {
+  //       topic.set('custom.fields.assigned_user', undefined);
+  //       topic.set('custom_fields.is_assigned', false);
+  //     },
 
-    }
-  });
+  //   }
+  // });
 }
 
 export default {
