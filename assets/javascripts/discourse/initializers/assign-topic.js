61015fca72fb1e6fe35e1687015dab094fe05eae
Jacob Shaw <jacob.shaw@genesys.com>
2022-01-21 11:21:02 -0500

    fleshed out plugin

---
+++ assets/javascripts/discourse/initializers/assign-topic.js
@@ -0,0 +1,53 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+function initWithApi(api) {
+  // if (!Discourse.SiteSettings.custom_directory_enabled) return;
+
+  const usersModel = api.container.lookup("controller:users-controller").get('model');
+  console.log('USERS MODEL', usersModel);
+
+  const defaultSearchTerm = '';
+
+  api.modifyClass("controller:topic", {
+    searchTerm: defaultSearchTerm,
+    actions: {
+
+      filterUsers(topic) {
+        const matchingUsers = usersModel?.filter((user) => {
+            return user.username?.toLowerCase()?.includes(topic.custom_fields.search_term.toLowerCase());
+        });
+        const userSearchList = document.getElementById('user-search-list');
+
+        userSearchList.innerHTML = matchingUsers.map((u) => {
+          return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
+        }).join('\n');
+
+        for (u in matchingUsers) {
+          const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
+
+          userEl.addEventListener("click", function() {
+            topic.set('custom_fields.assigned_user', u.username);
+            topic.set('custom_fields.is_assigned', true);
+            topic.set('custom_fields.search_term', '')
+            const event = new Event('change');  
+            document.getElementById('user-search').dispatchEvent(event);
+          });
+        }
+      },
+      
+      unassignUser(topic) {
+        topic.set('custom.fields.assigned_user', undefined);
+        topic.set('custom_fields.is_assigned', false);
+      },
+
+    }
+  });
+}
+
+export default {
+  name: "assign-topic",
+
+  initialize() {
+    withPluginApi("0.8", initWithApi);
+  }
+};
--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,59 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: agent-assign
+# about: assign topics to agents
 # version: 0.0.1
 # authors: Discourse
 # url: TODO
 # required_version: 2.7.0
 # transpile_js: true
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :agent_assign_enabled
 
 after_initialize do
+    if SiteSetting.agent_assign_enabled then
+
+        Topic.register_custom_field_type('assigned_user', :text)
+        Topic.register_custom_field_type('is_assigned', :boolean)
+        Topic.register_custom_field_type('search_term', :text)
+
+        # add to class and serializer to allow for default value for the setting
+        add_to_class(:topic, :assigned_user) do
+          if custom_fields['assigned_user'] != nil
+            custom_fields['assigned_user']
+          else
+            SiteSetting.signatures_visible_by_default
+          end
+        end
+
+        add_to_class(:topic, :is_assigned) do
+          if custom_fields['is_assigned'] != nil
+            custom_fields['is_assigned']
+          else
+            SiteSetting.signatures_visible_by_default
+          end
+        end
+
+        add_to_class(:topic, :search_term) do
+          if custom_fields['search_term'] != nil
+            custom_fields['search_term']
+          else
+            SiteSetting.signatures_visible_by_default
+          end
+        end
+    
+        add_to_serializer(:topic, :assigned_user) do
+          object.topic.custom_fields[assigned_user]
+        end
+
+        add_to_serializer(:topic, :is_assigned) do
+            object.topic.custom_fields[is_assigned]
+        end
+
+        add_to_serializer(:topic, :search_term) do
+            object.topic.custom_fields[search_term]
+        end
+    
+    end
+
 end
