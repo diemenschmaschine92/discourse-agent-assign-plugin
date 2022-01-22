a4d49e2a9dc75c6079296076be7bd9a1c6729e50
Jacob Shaw <jacob.shaw@genesys.com>
2022-01-21 23:18:58 -0500

    test adding findall to user model

--- assets/javascripts/discourse/connectors/topic-above-post-stream/assign-topic.js.es6
+++ assets/javascripts/discourse/connectors/topic-above-post-stream/assign-topic.js.es6
@@ -1,9 +1,11 @@
 import { withPluginApi } from 'discourse/lib/plugin-api';
+import User from 'discourse/models/user';
 
 function filterUsersWithApi(api, opts) {
   console.log('GOT IN FILTER ACTION');
   const { topic } = opts;
-  const users = api.container.lookup('controller:users')?.get('model') || {};
+  // const users = api.container.lookup('controller:users')?.get('model') || {};
+  const users = User.findAll();
   console.log('USERS', users);
   console.log('TOPIC', topic);
   const matchingUsers = users?.content?.filter((c) => {
@@ -13,14 +15,14 @@ function filterUsersWithApi(api, opts) {
   const userSearchList = document.getElementById('user-search-list');
 
   userSearchList.innerHTML = matchingUsers.map((u) => {
-    return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
+    return `<div class='${u.user?.username?.toLowerCase()}'>${u.user?.username}</div>`
   }).join('\n');
 
-  for (u in matchingUsers) {
-    const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
+  for (const u in matchingUsers) {
+    const userEl = userSearchList.getElementsByClassName(u.user?.username?.toLowerCase())[0];
 
     userEl.addEventListener("click", function() {
-      topic.set('assigned_user', u.username);
+      topic.set('assigned_user', u.user?.username);
       topic.set('is_assigned', true);
       topic.set('search_term', '')
       const event = new Event('change');  
--- assets/javascripts/discourse/initializers/assign-topic.js
+++ assets/javascripts/discourse/initializers/assign-topic.js
@@ -1,76 +1,43 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
+// import { withPluginApi } from "discourse/lib/plugin-api";
 import { ajax } from "discourse/lib/ajax";
-import { ID_CONSTRAINT } from "discourse/models/topic";
+import User from 'discourse/models/user';
 
-function initWithApi(api) {
-  console.log('INIT');
-  // if (!Discourse.SiteSettings.custom_directory_enabled) return;
 
-  // const usersController = Discourse.__container__.lookup('controller:users');
+// function initWithApi(api) {
+//   console.log('INIT');
+//   // if (!Discourse.SiteSettings.custom_directory_enabled) return;
 
-  api.modifyClass("route:topic", {
+//   // const usersController = Discourse.__container__.lookup('controller:users');
+//   User.reopen({
+//     findAll: function() {
+//       return ajax("/directory.json")
+//         .then((users) => {
+//           return users.map((u) => {
+//             console.log('USER FROM FIND ALL', User.create(u));
+//             return User.create(u);
+//         });
+//       });
+//     }
+//   });
 
-    model(params, transition) {
-      return ajax("/u?order=likes_received")
-        .then((response) => {
-          console.log('SUCCESSFUL LOAD');
-          if (params.slug.match(ID_CONSTRAINT)) {
-            transition.abort();
-      
-            DiscourseURL.routeTo(`/t/topic/${params.slug}/${params.id}`, {
-              replaceURL: true,
-            });
-      
-            return;
-          }
-      
-          const queryParams = transition.to.queryParams;
-      
-          let topic = this.modelFor("topic");
-          if (topic && topic.get("id") === parseInt(params.id, 10)) {
-            this.setupParams(topic, queryParams);
-            return topic;
-          } else {
-            let props = Object.assign({}, params);
-            delete props.username_filters;
-            delete props.filter;
-            topic = this.store.createRecord("topic", props);
-            return this.setupParams(topic, queryParams);
-          }
-        })
-        .catch((err) => {
-          if (params.slug.match(ID_CONSTRAINT)) {
-            transition.abort();
-      
-            DiscourseURL.routeTo(`/t/topic/${params.slug}/${params.id}`, {
-              replaceURL: true,
-            });
-      
-            return;
-          }
-      
-          const queryParams = transition.to.queryParams;
-      
-          let topic = this.modelFor("topic");
-          if (topic && topic.get("id") === parseInt(params.id, 10)) {
-            this.setupParams(topic, queryParams);
-            return topic;
-          } else {
-            let props = Object.assign({}, params);
-            delete props.username_filters;
-            delete props.filter;
-            topic = this.store.createRecord("topic", props);
-            return this.setupParams(topic, queryParams);
-          }
-        })
-    },
-  });
-}
+// }
 
 export default {
   name: "assign-topic",
 
   initialize() {
-    withPluginApi("0.8", initWithApi);
+    User.reopen({
+      findAll: function() {
+        return ajax("/directory.json")
+          .then((users) => {
+            return users.map((u) => {
+              console.log('USER FROM FIND ALL', User.create(u));
+              return User.create(u);
+          });
+        });
+      }
+    });
+    
+    //withPluginApi("0.8", initWithApi);
   }
 };
