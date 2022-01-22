7738b40760fa5b5b5819e54d78dd8a0d00f9841d
Jacob Shaw <jacob.shaw@genesys.com>
2022-01-21 21:48:00 -0500

    trigger user data load from topics route

--- assets/javascripts/discourse/initializers/assign-topic.js
+++ assets/javascripts/discourse/initializers/assign-topic.js
@@ -1,62 +1,74 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-// function initWithApi(api) {
-//   console.log('INIT');
-//   // if (!Discourse.SiteSettings.custom_directory_enabled) return;
+function initWithApi(api) {
+  console.log('INIT');
+  // if (!Discourse.SiteSettings.custom_directory_enabled) return;
 
-//   // const usersModel = api.container.lookup("controller:users")?.get('model') || {};
-//   // console.log('USERS', usersModel);
-//   // console.log('USERS MODEL', usersModel?.model);
+  // const usersController = Discourse.__container__.lookup('controller:users');
 
-//   //const defaultSearchTerm = '';
+  api.modifyClass("route:topic", {
 
-//   // api.modifyClass("controller:topic", {
-//   //   // searchTerm: defaultSearchTerm,
-//   // users: Ember.inject.controller("users"),
-
-//   //   actions: {
-
-//   //     filterUsers(topic) {
-//   //       console.log('GOT IN FILTER ACTION')
-//   //       console.log('USERS', users);
-//   //       console.log('TOPIC', topic);
-//   //       const matchingUsers = this.users?.filter((user) => {
-//   //           return user.username?.toLowerCase()?.includes(topic.custom_fields.search_term.toLowerCase());
-//   //       });
-//   //       const userSearchList = document.getElementById('user-search-list');
-
-//   //       userSearchList.innerHTML = matchingUsers.map((u) => {
-//   //         return `<div class='${u.username.toLowerCase()}'>${u.username}</div>`
-//   //       }).join('\n');
-
-//   //       for (u in matchingUsers) {
-//   //         const userEl = userSearchList.getElementsByClassName(u.username.toLowerCase())[0];
-
-//   //         userEl.addEventListener("click", function() {
-//   //           topic.set('custom_fields.assigned_user', u.username);
-//   //           topic.set('custom_fields.is_assigned', true);
-//   //           topic.set('custom_fields.search_term', '')
-//   //           const event = new Event('change');  
-//   //           document.getElementById('user-search').dispatchEvent(event);
-//   //         });
-//   //       }
-//   //     },
+    model(params, transition) {
+      return ajax("/u?order=likes_received")
+        .then((response) => {
+          console.log('')
+          if (params.slug.match(ID_CONSTRAINT)) {
+            transition.abort();
       
-//   //     unassignUser(topic) {
-//   //       topic.set('custom.fields.assigned_user', undefined);
-//   //       topic.set('custom_fields.is_assigned', false);
-//   //     },
-
-//   //   }
-//   // });
-// }
+            DiscourseURL.routeTo(`/t/topic/${params.slug}/${params.id}`, {
+              replaceURL: true,
+            });
+      
+            return;
+          }
+      
+          const queryParams = transition.to.queryParams;
+      
+          let topic = this.modelFor("topic");
+          if (topic && topic.get("id") === parseInt(params.id, 10)) {
+            this.setupParams(topic, queryParams);
+            return topic;
+          } else {
+            let props = Object.assign({}, params);
+            delete props.username_filters;
+            delete props.filter;
+            topic = this.store.createRecord("topic", props);
+            return this.setupParams(topic, queryParams);
+          }
+        })
+        .catch((err) => {
+          if (params.slug.match(ID_CONSTRAINT)) {
+            transition.abort();
+      
+            DiscourseURL.routeTo(`/t/topic/${params.slug}/${params.id}`, {
+              replaceURL: true,
+            });
+      
+            return;
+          }
+      
+          const queryParams = transition.to.queryParams;
+      
+          let topic = this.modelFor("topic");
+          if (topic && topic.get("id") === parseInt(params.id, 10)) {
+            this.setupParams(topic, queryParams);
+            return topic;
+          } else {
+            let props = Object.assign({}, params);
+            delete props.username_filters;
+            delete props.filter;
+            topic = this.store.createRecord("topic", props);
+            return this.setupParams(topic, queryParams);
+          }
+        })
+    },
+  });
+}
 
 export default {
   name: "assign-topic",
 
-  initialize(container) {
-    //withPluginApi("0.8", initWithApi);
-    const usersController = Discourse.__container__.lookup('controller:users');
-    usersController.loadUsers();
+  initialize() {
+    withPluginApi("0.8", initWithApi);
   }
 };
